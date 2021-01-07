e569879401f592206bab4f210f689e9d369fdbc7
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-01-07 18:44:14 +0200

    unfocus on submit

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '../';
 import './Search.css';
 
 const Search = ({ getData, isLoading }) => {
 	const [value, setValue] = useState('');
+	const [hasFocus, setHasFocus] = useState(false);
+	const inputRef = useRef(null);
 
 	const handleOnChange = e => {
 		setValue(e.target.value);
@@ -13,6 +15,7 @@ const Search = ({ getData, isLoading }) => {
 	const handleOnClick = () => {
 		if (!value.length) return;
 
+		setHasFocus(false);
 		getData(value);
 		setValue('');
 	};
@@ -23,9 +26,16 @@ const Search = ({ getData, isLoading }) => {
 		}
 	};
 
+	useEffect(() => {
+		const { current: input } = inputRef;
+		hasFocus ? input.focus() : input.blur();
+	}, [hasFocus]);
+
 	return (
 		<div className={'search__root'}>
 			<input
+				ref={inputRef}
+				onFocus={() => setHasFocus(true)}
 				value={value}
 				onChange={handleOnChange}
 				className={'search__input'}
