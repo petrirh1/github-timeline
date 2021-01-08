d40e33ad308ffe5229032472b2b8f6fff497a99c
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2021-01-08 19:31:15 +0200

    add semantic html

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import './Header.css';
 
 const Header = () => {
 	return (
-		<div className={'header__root'}>
+		<header className={'header__root'}>
 			<h1 className={'header__title'}>
 				<span className={'thin'}>GitHub </span>
 				Timeline
 			</h1>
-		</div>
+		</header>
 	);
 };
 
